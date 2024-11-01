1f2f8595c97a02e52aed8b29d4b4452b4860da5e
JLamb06 <jcwl2@kent.ac.uk>
2024-11-01 11:19:44 +0000

    Workshop 6

--- starterFile.py
+++ starterFile.py
@@ -1,17 +1,42 @@
 #Your code goes here. 
-
 import math
+def main():
+    a = float(input("Enter a number: "))
+    b = float(input("Enter a second number: "))
+    safe_divide(a,b)
+    input_list = []
+    process_list(input_list)
 
 def safe_divide(a, b):
+    try:
+        solution = a/b
+        print(solution)
+    except ZeroDivisionError:
+        print("Cannot Divide by zero")
+
+
 
 
 def process_list(input_list):
-
+    total = 0
+    x = 0
+    for x  in range(5): 
+        number = float(input("Enter a number: "))
+        input_list.append(number)
+        print(input_list)
+        try:
+            answer = int(input_list[x])**2
+            total += answer
+        except TypeError or ValueError:
+            print("That is not a valid number")
+    print(total)
+    
 
 def nested_operations(a, b):
-
+    print("")
 
 def process_input():
+    print("")
 
 
 main()
